977671f7c05b956de9406baee8ad2965fe0514a0
Cao Anh Quan <aquancva@gmail.com>
2018-07-31 12:12:31 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/graphiql/graphiql.css';
+import "./index.css";
+import registerServiceWorker from './registerServiceWorker';
+import GraphiQL from 'graphiql';
+
+const host = "http://localhost:8000";
+
+function graphQLFetcher(graphQLParams) {
+    // return fetch(host + '/graphql', {
+    //     method: 'post',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(graphQLParams),
+    // }).then(response => response.json());
+    return fetch(host + `/graphql?query=${graphQLParams.query}&variables=${graphQLParams.variables}`, {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' },
+        // body: JSON.stringify(graphQLParams),
+    }).then(response => response.json());
+}
+
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher} />, document.getElementById('root'));
+registerServiceWorker();

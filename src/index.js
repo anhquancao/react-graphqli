85e63aa34bf6c406f2a04eb463b4fa035d40305c
Cao Anh Quan <aquancva@gmail.com>
2018-08-02 11:17:30 +0700

    change graph method to post

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ import GraphiQL from 'graphiql';
 const host = window.host;
 
 function graphQLFetcher(graphQLParams) {
-    // return fetch(host + '/graphql', {
-    //     method: 'post',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(graphQLParams),
-    // }).then(response => response.json());
-    return fetch(host + `/graphql?query=${graphQLParams.query}&variables=${graphQLParams.variables}`, {
-        method: 'get',
+    return fetch(host + '/graphql', {
+        method: 'post',
         headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify(graphQLParams),
+        body: JSON.stringify(graphQLParams),
     }).then(response => response.json());
+    // return fetch(host + `/graphql?query=${graphQLParams.query}&variables=${graphQLParams.variables}`, {
+    //     method: 'get',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     // body: JSON.stringify(graphQLParams),
+    // }).then(response => response.json());
 }
 
 ReactDOM.render(<GraphiQL fetcher={graphQLFetcher} />, document.getElementById('root'));
